36176f4e3345f0e1f8e067b9c9474a83e923c279
QuadQuasimodo <pedromafernandes@outlook.pt>
2020-12-01 20:57:14 +0000

    Add ToString method

--- Projeto1_LP2/Planet.cs
+++ Projeto1_LP2/Planet.cs
@@ -45,5 +45,31 @@ namespace Projeto1_LP2
             MassRatio = massRT;
             EqTemperature = eqTemp;
         }
+
+        // Returns string with the Planet's Values in format determined by User
+        public string ToString(bool csv)
+        {
+            if (csv)
+            {
+                // Return values in CSV format
+                return Name + "," + HostName + "," + 
+                DiscoveryMethod + "," + DiscoveryYear + "," + 
+                OrbitPeriod + "," + RadiusRatio + "," + 
+                MassRatio + "," + EqTemperature;
+            }
+            else
+            {
+                return
+                "PLANET VALUES/n/n" +
+                $"Name: {Name}/n" + 
+                $"Host Star: {HostName}/n" +
+                $"Discovery Method: {DiscoveryMethod}/n" + 
+                $"Discovery Year: {DiscoveryYear}/n" + 
+                $"Orbit Period: {OrbitPeriod} days/n" + 
+                $"Radius (vs Earth): {RadiusRatio}/n" +
+                $"Mass (vs Earth): {MassRatio}/n" +
+                $"Equilibrium Temperature: {EqTemperature} kelvin/n";
+            }
+        }
     }   
 }
