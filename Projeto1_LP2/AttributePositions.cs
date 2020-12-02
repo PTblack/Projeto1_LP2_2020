bc0469f413cbf380a03db8412d04ae6857e9b79f
QuadQuasimodo <pedromafernandes@outlook.pt>
2020-12-02 20:20:36 +0000

    Create AttributePositions Enum

---
+++ Projeto1_LP2/AttributePositions.cs
@@ -0,0 +1,24 @@
+namespace Projeto1_LP2
+{
+    // Position of the relevant attribute in the csv file lines
+    public enum AttributePositions
+    {
+        // Planet Attributes
+        pl_namePOS = 0,
+        pl_hostNamePOS = 1,
+        pl_discMethodPOS = 12,
+        pl_discYearPOS = 13,
+        pl_orbPerPOS = 33,
+        pl_radePOS = 35,
+        pl_massPOS = 37,
+        pl_eqtPOS = 49,
+        // Star Attributes
+        st_teffPOS = 67,
+        st_radPOS = 68,
+        st_massPOS = 69,
+        st_agePOS = 74,
+        st_vsinPOS = 76,
+        st_rotpPOS = 77,
+        sy_distPOS = 92,
+    }
+}
