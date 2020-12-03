d4fea3b0ea9e98819b5f465005ec963e2596ccfa
FilipeVillar <filipevillar7@gmail.com>
2020-12-03 23:45:24 +0000

    Add reworks to the classes filemanager, planet and star
    This commit is trying to fix the classes and isn´t a final fix, it still needs more work

--- Projeto1_LP2/FileManager.cs
+++ Projeto1_LP2/FileManager.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using Microsoft.VisualBasic;
 
 namespace Projeto1_LP2
 {
@@ -14,11 +15,8 @@ namespace Projeto1_LP2
         private string fileFolder;
 
         // Collections
-        public Dictionary<string, Planet> Planets {get; set;}
-        public Dictionary<string, Star> Stars {get; set;}
-
-        private int maxPlanets;
-        private int maxStars;
+        private Dictionary<string, Planet> hashSetPL;
+        private HashSet<Star> hashSetST;
 
         // Number of Attributes specific to planet
         private int planetAttNum = 8;
@@ -29,128 +27,110 @@ namespace Projeto1_LP2
         public FileManager()
         {
             // Initialize the dictionaries for both planets and stars
-            Planets = new Dictionary<string, Planet>(maxPlanets);
-            Stars = new Dictionary<string, Star>(maxStars);
+            hashSetPL = new Dictionary<string, Planet>();
+            hashSetST = new HashSet<Star>();
 
             // Location of the folder that is going to be read
             fileFolder = Path.Combine(
                 Environment.GetFolderPath(
                     Environment.SpecialFolder.LocalApplicationData), 
                         applicationName);
-            
-            // Count the lines in the File, to see how many number of planets/stars there is 
-            OpenFile((line) => maxPlanets++);
-            OpenFile((line) => maxStars++);         
+                   
         }
-
-        private void OpenFile(Action<string> actionPerLine)
-        {
-            // Get the FileStream and the StreamReader...
-            using (FileStream fileStream = new FileStream(
-                fileFolder, FileMode.Open, FileAccess.Read))
-            {
-                using(StreamReader streamReader = new StreamReader(fileStream))
-                {
-                    string line;
-
-                    // Skip the first line of the folder.
-                    streamReader.ReadLine();
-
-                    // Read through every line.
-                    while((line = streamReader.ReadLine()) != null)
-                    {
-                        actionPerLine.Invoke(line);
-                    }
-                {
-                
-            {
-        {   
 
         // Searches File and creates Collection with 
         // planets and their wanted values
-        private void CreatePlanetCollection(Action<string> actionForEachPlanet)
+        private void CreatePlanetCollection(Func<string> actionForEachPlanet)
         {
             // Array that holds one planet's attributes as strings
             string[] planetAttributes = new string[8];
             // String representing line of the file
             string line;
 
-            // READS CSV FILE
-            using (StreamReader sr = new StreamReader(csvfile))
+            using (FileStream fileStream = new FileStream(
+                fileFolder, FileMode.Open, FileAccess.Read))
             {
-                // Skip the first 128 lines of the file
-                for(int i = 0; i >= 127; i++) {sr.ReadLine();};
-
-                // Read through every line until reaching empty line (end)
-                while((line = sr.ReadLine()) != null)
+                // READS CSV FILE
+                using (StreamReader sr = new StreamReader(file))
                 {
-                    // Turn line into string array (split csv line on ',')
-                    string[] attribs = line.Split(',');
+                    // Skip the first 128 lines of the file
+                    for (int i = 0; i <= 127; i++) { sr.ReadLine(); };
 
-                    /*
-                     * Select attributes significant to Planet and
-                     * add them to planetAttributes
-                     */
+                    // Read through every line until reaching empty line (end)
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // Turn line into string array (split csv line on ',')
+                        string[] attribs = line.Split(',');
+                        /*
+                         * Select attributes significant to Planet and
+                         * add them to planetAttributes
+                         */
 
-                    // Name
-                    planetAttributes [0] = attribs.ElementAt(
-                    (int)AttributePositions.pl_namePOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_namePOS) : 
-                        "Data Missing";
+                        // Name
+                        planetAttributes[0] = attribs.ElementAt(
+                        (int)AttributePositions.pl_namePOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_namePOS) :
+                            "Data Missing";
 
-                    // Host Name
-                    planetAttributes [1] = attribs.ElementAt(
-                    (int)AttributePositions.pl_hostNamePOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_hostNamePOS) : 
-                        "Data Missing";
+                        // Host Name
+                        planetAttributes[1] = attribs.ElementAt(
+                        (int)AttributePositions.pl_hostNamePOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_hostNamePOS) :
+                            "Data Missing";
 
-                    // Discovery Method
-                    planetAttributes [2] = attribs.ElementAt(
-                    (int)AttributePositions.pl_discMethodPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_discMethodPOS) : 
-                        "Data Missing";
+                        // Discovery Method
+                        planetAttributes[2] = attribs.ElementAt(
+                        (int)AttributePositions.pl_discMethodPOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_discMethodPOS) :
+                            "Data Missing";
 
-                    // Discovery Year
-                    planetAttributes [3] = attribs.ElementAt(
-                    (int)AttributePositions.pl_discYearPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_discYearPOS) : 
-                        "Data Missing";
+                        // Discovery Year
+                        planetAttributes[3] = attribs.ElementAt(
+                        (int)AttributePositions.pl_discYearPOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_discYearPOS) :
+                            "Data Missing";
 
-                    // Orbit Period
-                    planetAttributes [4] = attribs.ElementAt(
-                    (int)AttributePositions.pl_orbPerPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_orbPerPOS) : 
-                        "Data Missing";
+                        // Orbit Period
+                        planetAttributes[4] = attribs.ElementAt(
+                        (int)AttributePositions.pl_orbPerPOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_orbPerPOS) :
+                            "Data Missing";
 
-                    // Radius
-                    planetAttributes [5] = attribs.ElementAt(
-                    (int)AttributePositions.pl_radePOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_radePOS) : 
-                        "Data Missing";
+                        // Radius
+                        planetAttributes[5] = attribs.ElementAt(
+                        (int)AttributePositions.pl_radePOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_radePOS) :
+                            "Data Missing";
 
-                    // Mass
-                    planetAttributes [6] = attribs.ElementAt(
-                    (int)AttributePositions.pl_massPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_massPOS) : 
-                        "Data Missing";
+                        // Mass
+                        planetAttributes[6] = attribs.ElementAt(
+                        (int)AttributePositions.pl_massPOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_massPOS) :
+                            "Data Missing";
 
-                    // Equilibrium Temperature
-                    planetAttributes [7] = attribs.ElementAt(
-                    (int)AttributePositions.pl_eqtPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.pl_eqtPOS) : 
-                        "Data Missing";
+                        // Equilibrium Temperature
+                        planetAttributes[7] = attribs.ElementAt(
+                        (int)AttributePositions.pl_eqtPOS) != null ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_eqtPOS) :
+                            "Data Missing";
 
-                    // Add planetAttributes array to collection
-                    
+                        Planet p = new Planet(planetAttributes[0], planetAttributes[1], planetAttributes[2], planetAttributes[3],
+                                planetAttributes[4], planetAttributes[5], planetAttributes[6], planetAttributes[7]);
+
+
+
+                    }
+
                 }
+
             }
         }
 
@@ -230,6 +210,7 @@ namespace Projeto1_LP2
                         "Data Missing";
 
                     // Add planetAttributes array to collection
+                    
                 }
             }
         }
