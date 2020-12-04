52ea2573ac62c70391fa72f245c8d7501005420b
QuadQuasimodo <pedromafernandes@outlook.pt>
2020-12-04 21:59:52 +0000

    Develop CreateStarCollection and Create FindValAttributeIndex

--- Projeto1_LP2/FileManager.cs
+++ Projeto1_LP2/FileManager.cs
@@ -10,28 +10,35 @@ namespace Projeto1_LP2
 {
     class FileManager
     {
-        private string file; // file name for now, we will need to change this to the argument passed by the user
+        private string file;
         private string fileFolder;
 
         // Collections
-        private HashSet<Planet> HashsetPL;
-        private HashSet<Star> hashSetST;
+        private HashSet<Planet> HashSetPL;
+        private HashSet<Star> HashSetST;
 
         public FileManager(string file)
         {
             this.file = file;
-            // Initialize the dictionaries for both planets and stars
-            HashsetPL = new HashSet<Planet>();
-            hashSetST = new HashSet<Star>();
+            // Initialize the collections for planets and stars
+            HashSetPL = new HashSet<Planet>();
+            HashSetST = new HashSet<Star>();
 
             // Location of the folder that is going to be read
             fileFolder = Path.Combine(
                 Environment.GetFolderPath(
-                    Environment.SpecialFolder.Desktop), 
-                        file);
+                Environment.SpecialFolder.Desktop), file);
 
-            CreatePlanetCollection();                   
+            CreatePlanetCollection();
+            // CreateStarCollection(); 
         }
+
+        /* 
+         * >>> QUESTION <<<
+         * Can't this be a property that the UI "gets" from here?
+         * >>> QUESTION <<<
+         */
+        public HashSet<Planet> ReturnPlanet() => HashSetPL;
 
         // Searches File and creates Collection with 
         // planets and their wanted values
@@ -42,7 +49,6 @@ namespace Projeto1_LP2
             // String representing line of the file
             string line;
 
-
             using (FileStream fileStream = new FileStream(
                 fileFolder, FileMode.Open, FileAccess.Read))
             {
@@ -50,14 +56,14 @@ namespace Projeto1_LP2
                 using (StreamReader sr = new StreamReader(file))
                 {
                     // Skip the first 128 lines of the file
-                    for (int i = 0; i <= 127; i++) 
-                        sr.ReadLine();
+                    for (int i = 0; i <= 127; i++) sr.ReadLine();
 
                     // Read through every line until reaching empty line (end)
                     while ((line = sr.ReadLine()) != null)
                     {
                         // Turn line into string array (split csv line on ',')
                         string[] attribs = line.Split(',');
+
                         /*
                          * Select attributes significant to Planet and
                          * add them to planetAttributes
@@ -68,149 +74,213 @@ namespace Projeto1_LP2
                         (int)AttributePositions.pl_namePOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_namePOS) :
-                            "N/A";
+                                "N/A";
 
                         // Host Name
                         planetAttributes[1] = attribs.ElementAt(
                         (int)AttributePositions.pl_hostNamePOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_hostNamePOS) :
-                            "N/A";
+                                "N/A";
 
                         // Discovery Method
                         planetAttributes[2] = attribs.ElementAt(
                         (int)AttributePositions.pl_discMethodPOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_discMethodPOS) :
-                            "N/A";
+                                "N/A";
 
                         // Discovery Year
                         planetAttributes[3] = attribs.ElementAt(
                         (int)AttributePositions.pl_discYearPOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_discYearPOS) :
-                            "N/A";
+                                "N/A";
 
                         // Orbit Period
                         planetAttributes[4] = attribs.ElementAt(
                         (int)AttributePositions.pl_orbPerPOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_orbPerPOS) :
-                            "N/A";
+                                "N/A";
 
                         // Radius
                         planetAttributes[5] = attribs.ElementAt(
                         (int)AttributePositions.pl_radePOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_radePOS) :
-                            "N/A";
+                                "N/A";
 
                         // Mass
                         planetAttributes[6] = attribs.ElementAt(
                         (int)AttributePositions.pl_massPOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_massPOS) :
-                            "N/A";
+                                "N/A";
 
                         // Equilibrium Temperature
                         planetAttributes[7] = attribs.ElementAt(
                         (int)AttributePositions.pl_eqtPOS) != "" ?
                             attribs.ElementAt(
                                 (int)AttributePositions.pl_eqtPOS) :
-                            "N/A";
+                                "N/A";
 
-                        Planet p = new Planet(planetAttributes[0], planetAttributes[1], planetAttributes[2], planetAttributes[3],
-                                planetAttributes[4], planetAttributes[5], planetAttributes[6], planetAttributes[7]);
+                        Planet p = new Planet(
+                            planetAttributes[0], planetAttributes[1], 
+                            planetAttributes[2], planetAttributes[3],
+                            planetAttributes[4], planetAttributes[5], 
+                            planetAttributes[6], planetAttributes[7]);
 
-                        HashsetPL.Add(p);
+                        HashSetPL.Add(p);
                     }
                 }
-
             }
         }
 
         // Searches File and creates Collection with 
         // stars and their wanted values
-        private void CreateStarCollection(Action<string> actionForEachStar)
+        private void CreateStarCollection()
         {
-            // Array that holds one star's attributes as strings
-            string[] starAttributes = new string[8];
+            // Array that holds one line's important attributes as strings
+            string[] starAttributes = new string[10];
             // String representing line of the file
             string line;
 
-            // READS CSV FILE
-            using (StreamReader sr = new StreamReader(file))
+            using (FileStream fileStream = new FileStream(
+                fileFolder, FileMode.Open, FileAccess.Read))
             {
-                // Skip the first 128 lines of the folder.
-                for(int i = 0; i >= 127; i++) {sr.ReadLine();};
-
-                // Read through every line until reaching end of file
-                while((line = sr.ReadLine()) != null)
+                // READS CSV FILE
+                using (StreamReader sr = new StreamReader(file))
                 {
-                    // Turn line into string array (split csv line on ',')
-                    string[] attribs = line.Split(',');
+                    // Skip the first 128 lines of the folder.
+                    for(int i = 0; i >= 127; i++) sr.ReadLine();
 
-                    /*
-                     * Select attributes significant to Star and
-                     * add them to starAttributes
-                     */
+                    // Read through every line until reaching end of file
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        // Turn line into string array (split csv line on ',')
+                        string[] attribs = line.Split(',');
 
-                    // Effective Temperature
-                    starAttributes [0] = attribs.ElementAt( 
-                    (int)AttributePositions.st_teffPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.st_teffPOS) : 
-                        "Data Missing";
+                        /*
+                        * Select attributes significant to Star and
+                        * add them to starAttributes
+                        */
 
-                    // Star Radius
-                    starAttributes[1] = attribs.ElementAt(
-                    (int)AttributePositions.st_radPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.st_radPOS) : 
-                        "Data Missing";
+                        // Hosted Planet
+                        starAttributes[0] = attribs.ElementAt(
+                        (int)AttributePositions.pl_namePOS) != "" ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_namePOS) :
+                                "N/A";
 
-                    // Star Mass
-                    starAttributes[2] = attribs.ElementAt(
-                    (int)AttributePositions.st_massPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.st_massPOS) : 
-                        "Data Missing";
+                        // Discovery Method
+                        starAttributes[1] = attribs.ElementAt(
+                        (int)AttributePositions.pl_discMethodPOS) != "" ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_discMethodPOS) :
+                                "N/A";
 
-                    // Star Age
-                    starAttributes[3] = attribs.ElementAt(
-                    (int)AttributePositions.st_agePOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.st_agePOS) : 
-                        "Data Missing";
+                        // Discovery Year
+                        starAttributes[2] = attribs.ElementAt(
+                        (int)AttributePositions.pl_discYearPOS) != "" ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.pl_discYearPOS) :
+                                "N/A";
+                        
+                        // Effective Temperature
+                        starAttributes[3] = attribs.ElementAt(
+                        (int)AttributePositions.st_teffPOS) != "" ?
+                            attribs.ElementAt(
+                                (int)AttributePositions.st_teffPOS) :
+                                "N/A";
 
-                    // Star Rotation Velocity
-                    starAttributes[4] = attribs.ElementAt(
-                    (int)AttributePositions.st_vsinPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.st_vsinPOS) : 
-                        "Data Missing";
+                        // Star Radius
+                        starAttributes[4] = attribs.ElementAt(
+                        (int)AttributePositions.st_radPOS) != "" ? 
+                            attribs.ElementAt(
+                                (int)AttributePositions.st_radPOS) : 
+                                "N/A";
 
-                    // Star Rotation Period
-                    starAttributes[5] = attribs.ElementAt(
-                    (int)AttributePositions.st_rotpPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.st_rotpPOS) : 
-                        "Data Missing";
+                        // Star Mass
+                        starAttributes[5] = attribs.ElementAt(
+                        (int)AttributePositions.st_massPOS) != "" ? 
+                            attribs.ElementAt(
+                                (int)AttributePositions.st_massPOS) : 
+                                "N/A";
 
-                    // Distance to Sun
-                    starAttributes[6] = attribs.ElementAt(
-                    (int)AttributePositions.sy_distPOS) != null? 
-                        attribs.ElementAt(
-                            (int)AttributePositions.sy_distPOS) : 
-                        "Data Missing";
+                        // Star Age
+                        starAttributes[6] = attribs.ElementAt(
+                        (int)AttributePositions.st_agePOS) != "" ? 
+                            attribs.ElementAt(
+                                (int)AttributePositions.st_agePOS) : 
+                                "N/A";
 
-                    // Add planetAttributes array to collection
-                    
+                        // Star Rotation Velocity
+                        starAttributes[7] = attribs.ElementAt(
+                        (int)AttributePositions.st_vsinPOS) != "" ? 
+                            attribs.ElementAt(
+                                (int)AttributePositions.st_vsinPOS) : 
+                                "N/A";
+
+                        // Star Rotation Period
+                        starAttributes[8] = attribs.ElementAt(
+                        (int)AttributePositions.st_rotpPOS) != "" ? 
+                            attribs.ElementAt(
+                                (int)AttributePositions.st_rotpPOS) : 
+                                "N/A";
+
+                        // Distance to Sun
+                        starAttributes[9] = attribs.ElementAt(
+                        (int)AttributePositions.sy_distPOS) != "" ? 
+                            attribs.ElementAt(
+                                (int)AttributePositions.sy_distPOS) : 
+                                "N/A";
+
+                        Star s = new Star(
+                                starAttributes[0], starAttributes[1], 
+                                starAttributes[2], starAttributes[3],
+                                starAttributes[4], starAttributes[5], 
+                                starAttributes[6], starAttributes[7],
+                                starAttributes[8], starAttributes[9]);
+
+                        HashSetST.Add(s);
+                    }
                 }
             }
         }
 
-        public HashSet<Planet> ReturnPlanet() => HashsetPL;
-        
+        // Find and save position of columns with wanted attributes
+        // IN CONSTRUCTION
+         private void FindValAttributeIndex()
+        {
+            // Array that holds one line's important attributes as strings
+            string[] valAttributes = new string[15];
+            // String representing line of the file
+            string attributeline;
+
+            using (FileStream fileStream = new FileStream(
+                fileFolder, FileMode.Open, FileAccess.Read))
+            {
+                // READS CSV FILE
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    // Skip the first 127 lines of the folder.
+                    for(int i = 0; i >= 126; i++) sr.ReadLine();
+
+                    // Read and save line with attribute columns
+                    attributeline = sr.ReadLine();
+
+                    // Create array from column lines
+                    string[] attribs = attributeline.Split(',');
+
+                    /*
+                     * >>> TO DO <<<
+                     * find important things and create collection with 
+                     * valuable coordinates to be used in Create[BLANK]Collection
+                     * >>> TO DO <<<
+                     */
+                }
+            }
+        }
     }
 }
