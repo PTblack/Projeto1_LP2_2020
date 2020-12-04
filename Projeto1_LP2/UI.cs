fc5fe61df775764f0863be98e8f51f14cd395e09
QuadQuasimodo <pedromafernandes@outlook.pt>
2020-12-04 21:56:00 +0000

    Add string formatting

--- Projeto1_LP2/UI.cs
+++ Projeto1_LP2/UI.cs
@@ -46,49 +46,63 @@ namespace Projeto1_LP2
             stringArgs =new Dictionary<string, string>();
             floatArgs =new Dictionary<string, float>();
 
+            // Search Criteria
             boolArgs.Add("-search-planet", false);
             boolArgs.Add("-search-star", false);
             boolArgs.Add("-planet-info", false);
             boolArgs.Add("-star-info", false);
 
+            // Names
             stringArgs.Add("-file", "");
             stringArgs.Add("-planet-name", "");
             stringArgs.Add("-star-name", "");
             stringArgs.Add("-host-name", "");
 
+            // Temperature
             floatArgs.Add("-star-temp-min", 0);
             floatArgs.Add("-star-temp", 0);
             floatArgs.Add("-planet-temp-min", 0);
             floatArgs.Add("-planet-temp", 0);
 
+            // Radius
             floatArgs.Add("-star-rade-min", 0);
             floatArgs.Add("-star-rade", 0);
             floatArgs.Add("-planet-rade-min", 0);
             floatArgs.Add("-planet-rade", 0);
 
+            // Mass
             floatArgs.Add("-star-mass-min", 0);
             floatArgs.Add("-star-mass", 0);
             floatArgs.Add("-planet-mass-min", 0);
             floatArgs.Add("-planet-mass", 0);
             
+            // Rotation Velocity
             floatArgs.Add("-star-vsin-min", 0);
             floatArgs.Add("-star-vsin", 0);
 
+            // Rotation Period
             floatArgs.Add("-star-rotp-min", 0);
             floatArgs.Add("-star-rotp", 0);
 
+            // Star Age
             floatArgs.Add("-star-age-min", 0);
             floatArgs.Add("--star-age", 0);
 
+            // Planet Orbital Period
             floatArgs.Add("-planet-orbper-min", 0);
             floatArgs.Add("-planet-orbper", 0);
 
+            // Discovery Specs
             floatArgs.Add("-disc-year", 0);
             floatArgs.Add("-disc-method", 0);
 
+            // Star Distance to Sun
             floatArgs.Add("-sy-dist", 0);
         }
-        public void Show()
+
+        // PASSAR PARA CLASSE DE UI NOVA QUE FOR CRIADA
+        // Show collection of planets that fit the user's requests
+        public void ShowPlCollection()
         {
             Options();
             fm = new FileManager(stringArgs["-file"]);
@@ -96,10 +110,10 @@ namespace Projeto1_LP2
             PlanetCollection = fm.ReturnPlanet();
             foreach (Planet p in PlanetCollection)
             {
-                Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7}", p.Name,
-                    p.HostName, p.DiscoveryMethod, p.DiscoveryYear,
-                    p.OrbitPeriod, p.RadiusRatio, p.MassRatio,
-                    p.EqTemperature);
+                Console.WriteLine(
+                    "{0} / {1} / {2} / {3} / {4:f4} / {5:f4} / {6:f4} / {7:f4}", 
+                    p.Name, p.HostName, p.DiscoveryMethod, p.DiscoveryYear,
+                    p.OrbitPeriod, p.RadiusRatio, p.MassRatio, p.EqTemperature);
             }               
         }
     }
