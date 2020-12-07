6eee32e95b93575208542a83f90303a3d677f61c
QuadQuasimodo <pedromafernandes@outlook.pt>
2020-12-07 16:42:11 +0000

    Add AttribNumFluct error to ExceptionManager and ErrorCodes

--- Projeto1_LP2/ExceptionManager.cs
+++ Projeto1_LP2/ExceptionManager.cs
@@ -2,21 +2,30 @@ using System;
 
 namespace Projeto1_LP2
 {
-    public class ExceptionManager
+    public static class ExceptionManager
     {
 
-        // Talvez se passe para um delegate Action que recebe um int
-        public void ExceptionControl(int errorCode)
+        // Talvez se passe para um delegate Action que recebe um int?
+        public static void ExceptionControl(ErrorCodes errorCodes)
         {
-            switch(errorCode)
+            switch(errorCodes)
             {
-                case (int)ErrorCodes.AttribsMissing:
+                case ErrorCodes.AttribsMissing:
+                    Console.WriteLine("Error: Attributes Missing");
+                    
                     Console.WriteLine(
-                        "Attributes \"pl_name\" and \"hostname\"" + 
-                        "missing in given file");
+                        "Attributes \"pl_name\" and/or \"hostname\"" + 
+                        "are missing in given file");
+                    break;
+                
+                case ErrorCodes.AttribNumFluct:
+                    Console.WriteLine("Error: Attribute Number Fluctuation");
+                    
+                    Console.WriteLine(
+                        "The given file has line(s) that don't match the " + 
+                        "number of attributes stated in the file header");
                     break;
             }
-
         }
     }
 }
